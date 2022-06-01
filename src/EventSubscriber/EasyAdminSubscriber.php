cb5a59dcd860c7c6c7b241fb4df6599d71b7da16
mlachand <lachand_michael@hotmail.com>
2022-06-01 10:48:02 +0200

    add eventAdmin and Hash password on admin user create/update

---
+++ src/EventSubscriber/EasyAdminSubscriber.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityPersistedEvent;
+use EasyCorp\Bundle\EasyAdminBundle\Event\BeforeEntityUpdatedEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class EasyAdminSubscriber implements EventSubscriberInterface
+{
+
+    public function __construct( private UserPasswordHasherInterface $userPasswordHasher)
+    {
+    }
+
+    public function onBeforeEntityPersistedEvent(BeforeEntityPersistedEvent $event): void
+    {
+        $entityInstance = $event->getEntityInstance();
+
+        if ( $entityInstance instanceof User && $entityInstance->plainPassword) {
+            $hashedPassword = $this->userPasswordHasher->hashPassword($entityInstance, $entityInstance->plainPassword);
+
+            $entityInstance->setPassword($hashedPassword);
+        }
+    }
+
+    public function onBeforeEntityUpdatedEvent(BeforeEntityUpdatedEvent $event): void
+    {
+        $entityInstance = $event->getEntityInstance();
+
+        if ( $entityInstance instanceof User) {
+
+        }
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            BeforeEntityPersistedEvent::class => 'onBeforeEntityPersistedEvent',
+            BeforeEntityUpdatedEvent::class => 'onBeforeEntityUpdatedEvent',
+        ];
+    }
+}
