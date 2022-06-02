83f60dea1a0ebc9b2c5307d8924a039d3cd4c7d0
mlachand <lachand_michael@hotmail.com>
2022-06-02 16:58:59 +0200

    add Requirement on Symfony 6.1

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Requirement\Requirement;
 
 class PostsController extends AbstractController
 {
@@ -37,19 +38,17 @@ class PostsController extends AbstractController
         return $this->render('posts/index.html.twig', compact('pagination'));
     }
 
-    #[Route(path:'/posts/{year}/{month}/{day}/{slug}',
+    #[Route(path:'/posts/{date}/{slug}',
             name: 'app_posts_show',
             requirements: [
-                'year' => '[0-9]{4}',
-                'month' => '[0-9]{2}',
-                'day' => '[0-9]{2}',
-                'slug' => '[a-z0-9\-]+',
+                'date' => Requirement::DATE_YMD,
+                'slug' => Requirement::ASCII_SLUG
            ] ,
             methods: ['GET'])]
-    public function show(int $year, int $month, int $day, string $slug): Response
+    public function show(string $date, string $slug): Response
     {
 
-        $post = $this->postRepository->findOneByPublishDateAndSlug($year, $month, $day, $slug);
+        $post = $this->postRepository->findOneByPublishDateAndSlug($date, $slug);
 
         if(is_null($post)){
             throw $this->createNotFoundException();
